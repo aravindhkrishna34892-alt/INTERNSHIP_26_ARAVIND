b9ecc031c4f4402585cbb90d45b3c5025913d0af
Aravind Krishna C A <aravindhkrishna34892@gmail.com>
2026-06-18 05:36:09 +0530

    Add files via upload

---
+++ Full_adder/project_1.srcs/sim_1/new/full_adder_tb.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.06.2026 10:37:12
+// Design Name: 
+// Module Name: full_adder_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module full_adder_tb;
+ reg A;
+ reg B;
+ reg CIN;
+ wire SUM;
+ wire COUT;
+
+ 
+ full_adder dut(
+  
+    
+    
+endmodule
